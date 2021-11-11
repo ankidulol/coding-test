b528d52a9eaa27d0b9739db00b3517e12f38724e
jrvq <jarvinen.joonass@gmail.com>
2021-11-11 08:16:44 +0200

    test test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
     <div>
       <h1>Coding Test</h1>
-      <h2>Test</h2>
-      <h2>Test</h2>
-      <h2>Test</h2>
-      <h2>Test</h2>
     </div>
   );
 }
